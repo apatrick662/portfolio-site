0babf099b118d24cae514ec0c89340baff3c438b
adam <apatrick662>
2019-04-10 07:36:56 -0500

    added js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-bodyDiv = document.getElementById("body-wrapper");
+var bodyDiv = document.getElementById("body-wrapper");
 
 function addDiv() {
-    
+    var newDiv = document.createElement("div");
+    newDiv.classList.add('body-entry');
+    var newUl = document.createElement("ul");
+    var link = document.createElement("li");
+    var linkText = document.createTextNode(document.getElementById("linkName").value);
+    var desc = document.createElement("li");
+    var descText = document.createTextNode(document.getElementById("desc").value);
 }
